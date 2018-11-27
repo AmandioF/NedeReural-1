4c23f6f5264f21148eaef02554128de05a69dc5b
AmandioF <amandio31123@gmail.com>
2018-11-27 01:27:59 -0300

    Update inputOutput.pl

--- Prolog/src/inputOutput.pl
+++ Prolog/src/inputOutput.pl
@@ -2,48 +2,100 @@
 
 aaa.
 
-% ----------------- Parte com funçoes de Escrita ----------------------------
+%% ----------------- Parte com funçoes de Escrita ----------------------------
+
+% -- Escreve os dados da biases e do output
+% nos seus respectivos arquivos
+writeData(BH, BO, WH, WO):-
+    writeFile("biasesHidden.txt", BH),
+    writeFile("weightsHidden.txt", WH),
+
+    writeFile("biasesOut.txt", BO),
+    writeFile("weightsOut.txt", WO).
+
 % Escreve no arquivo
 writeFile(File, Matrix):-
-    % Transforma a matriz em string
     matrix_to_list(Matrix, Arr),
-    atomic_list_concat(Arr, '\n', Str),
-    
-    % Escreve a string no arquivo
-    open(File, write, Stream),
-    write(Stream, Str), nl,
+
+    clear(File),
+    open(File, append, Stream),
+    processW(Stream, Arr), nl,
     close(Stream).
+
+% Cria uma lista com os caracteres presentes no Arquivo
+processW(_, []).
+processW(Stream, [Line|Rest]) :-
+    write(Stream, Line), nl,
+    processW(Stream, Rest).
 
 % Transforma as listas da matriz em string
 matrix_to_list([], []).
 matrix_to_list([H|T], [C|R]):- 
     atomic_list_concat(H, " ", C),
     matrix_to_list(T, R).
+
+clear(File):- open(File, write, Stream), write(Stream, ""), close(Stream).
     
 % ------------------ Parte com funçoes de Leitura -------------------------
+
+% Retorna a matriz de Treino
+getTrainings(Train):- readTraining("trainings.txt", Train).
+
+% Retorna a matriz de Testes
+getTests(Train):- readTraining("tests.txt", Train).
+
+% -- Retorna dados da Hidden
+% Retorna a matriz de Treino
+getBiasesHidden(Train):- readFile("biasesHidden.txt", Train).
+% Retorna a matriz de Treino
+getWeightsHidden(Train):- readFile("weightsHidden.txt", Train).
+
+% -- Retorna dados do output
+% Retorna a matriz de Treino
+getBiasesOut(Train):- readFile("biasesOut.txt", Train).
+% Retorna a matriz de Treino
+getWeightsOut(Train):- readFile("biasesOut.txt", Train).
 
 % Lê o arquivo e transforma-o em uma matriz de numeros
 readFile(File, Arr):-
     open(File, read, Stream),
 
-    get_char(Stream, Char1),
+    read_line_to_codes(Stream, Line),
 
-    % Transforma o Arquivo em matriz
-    process(Char1, Stream, F),
-    atomic_list_concat(F, Juntando),
-    split_string(Juntando, "\n", "\n", Separando),
-    arr_to_matrix(Separando, Arr),
+    % Setar a quantidade de linhas a ser lida
+    processR(Line, Stream, F, 0, 600),
+    arr_to_matrix(F, Arr),
+    close(Stream).
+
+% Lê um arquivo em forma de Treino
+readTraining(File, Arr):-
+    open(File, read, Stream),
+
+    read_line_to_codes(Stream, Line),
+
+    % Setar a quantidade de linhas a ser lida
+    processR(Line, Stream, F, 0, 600),
+    arr_to_training(F, Arr),
     close(Stream).
 
 % Transforma as strings ,presentes na lista, em listas
 arr_to_matrix([], []).
 arr_to_matrix([H|T], [C|R]) :- string_to_arr(H, C), arr_to_matrix(T, R).
 
+% Transforma as strings ,presentes na lista, em listas
+arr_to_training([], []).
+arr_to_training([H|T], [C|R]) :- 
+    string_to_arr(H, [Label|Mt]),
+    C = [Label, Mt],
+    arr_to_training(T, R).
+
 % Cria uma lista com os caracteres presentes no Arquivo
-process(end_of_file, _, []).
-process(Char, Stream, [Char|Ans]) :-
-    get_char(Stream, Char1), 
-    process(Char1, Stream, Ans).
+processR(end_of_file, _, [], _, _).
+processR(_, _, [], Limit, Limit).
+processR(Line, Stream, [Line|Ans], Cnt, Limit) :-
+    read_line_to_codes(Stream, Line1),
+    Bef is Cnt + 1,
+    processR(Line1, Stream, Ans, Bef, Limit).
 
 % Transforma todos os caracteres em Atomos
 arr_to_atom([], []).
@@ -59,10 +111,6 @@ string_to_arr(Str, Arr):-
     
     arr_to_atom(Aux1 , Aux2),
     arr_to_number(Aux2, Arr).
-
-getTest(TestSet) :- TestSet = [].
-
-getTraining(TrainingSet) :- TrainingSet = [].
 
 printEpoch(CorrectAmount, Amount, TotalAmount) :- write("Epoch #"),
                                                   write(Amount),
